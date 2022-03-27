4d1e9cccc55d34efd8751723251c960262661496
James Patrick Soldao <102235060+jasoldao@users.noreply.github.com>
2022-03-27 22:08:38 +0800

    Update LandVehicleTester.java

--- LandVehicleTester.java
+++ LandVehicleTester.java
@@ -1,8 +1,7 @@
 package me2;
 
-// Di ko pa mafigure out paano mapapalabas yung sound nung mga engines ng SUV at Motorcycle, although may idea na me double check ko lang
 public class LandVehicleTester {
-	public void startEngine() {
-		
-	}
+  public void startEngine(){
+    
+  }
 }
