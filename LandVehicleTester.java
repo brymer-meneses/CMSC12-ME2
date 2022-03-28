bfeff320b43e2ef633c3c8d1a27f58244398a2d0
James Patrick Soldao <102235060+jasoldao@users.noreply.github.com>
2022-03-28 11:48:36 +0800

    Update LandVehicleTester.java

--- LandVehicleTester.java
+++ LandVehicleTester.java
@@ -1,8 +1,7 @@
 package me2;
 
-//TODO: Print the engine sounds from Motorcycle and SUV
 public class LandVehicleTester {
   public void startEngine(Vehicle v){
-    System.out.println();
+    v.makeSound();
   }
 }
